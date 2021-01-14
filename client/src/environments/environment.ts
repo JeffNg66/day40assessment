56500d89d1d1926654fe22d12a2d4a6d6ec45e85
Jeff Ng <jeff-ng@hotmail.com>
2021-01-14 22:15:27 +0800

    Vercel Deployment

--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
-  // api_url: 'https://jn-mymap.herokuapp.com'
-  api_url: 'http://localhost:3000'
+  api_url: 'https://jn-mymap.herokuapp.com'
+  // api_url: 'http://localhost:3000'
 };
 
 /*
